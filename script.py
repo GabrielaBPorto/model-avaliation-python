fc9c4ec8875a6bebb7a994f1f9310420d7e81a00
Gabriela Porto <gabriela.bairros.porto96@gmail.com>
2023-08-24 16:29:35 -0300

    feature: read of txt files

--- script.py
+++ script.py
@@ -1,13 +1,13 @@
-# Tamanhos das imagens que voce quer ler
+import os
+
+
 tamanhos = [512, 608, 800]
 
-# Loop para passar por cada tamanho
+caminho_atual = os.getcwd()
+
 for tamanho in tamanhos:
-    # Criar o caminho do arquivo usando o tamanho
-    caminho_do_arquivo = './' + str(tamanho) + '_Yolov4_5000_dados_celulas.txt'
+    caminho_do_arquivo = os.path.join(caminho_atual + '/'+ str(tamanho) + '_yolov4_5000_dados_celulas.txt')
     
-    # Abrir o arquivo no modo de leitura
     with open(caminho_do_arquivo, 'r') as arquivo:
-        # Ler e imprimir cada linha do arquivo
         for linha in arquivo:
             print(linha)
